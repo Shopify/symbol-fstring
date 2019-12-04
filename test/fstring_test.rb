37a982f96aeb81256edf722f7e4e9f81c649c6d6
Jean Boussier <jean.boussier@gmail.com>
2019-12-04 13:50:06 +0100

    Implement FString.patch_symbol! to replace Symbol#to_s

--- test/fstring_test.rb
+++ test/fstring_test.rb
@@ -22,10 +22,26 @@ class FStringTest < Minitest::Test
     assert_fstring fstring
   end
 
+  def test_symbol_to_s_is_patched
+    with_patch do
+      fstring = :foo.to_s
+      assert_equal 'foo', fstring
+      assert_equal Encoding::US_ASCII, fstring.encoding
+      assert_fstring fstring
+    end
+  end
+
   private
 
   def assert_fstring(string)
     description = JSON.parse(ObjectSpace.dump(string))
     assert description["fstring"], "Expected #{string.inspect} to be interned, but wasn't"
+  end
+
+  def with_patch
+    FString.patch_symbol!
+    yield
+  ensure
+    FString.unpatch_symbol!
   end
 end
